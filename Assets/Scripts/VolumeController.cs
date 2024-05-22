240ecda182fb4cb83c3892f50498c22cb7c81aad
L30Dzzz <113184803+L30Dzzz@users.noreply.github.com>
2024-05-22 12:42:49 -0400

    Vol SLider done

---
+++ Assets/Scripts/VolumeController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeController : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public float currentVolume;
+    public AudioSource gameAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider.value = 1;
+        volumeSlider.onValueChanged.AddListener(delegate {ValueChangeCheck(); });
+        Debug.Log(volumeSlider.value);
+    }
+
+    void ValueChangeCheck()
+    {
+        gameAudio.volume = volumeSlider.value;
+        Debug.Log(volumeSlider.value);
+    }
+}
